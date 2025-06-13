4fb56eaa4f204bc46152966511533f23e2eefb43
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2025-06-12 17:45:19 -0700

    laserbeamfoam runs in parallel (4mpi)

--- slurm_run_sif.sh
+++ slurm_run_sif.sh
@@ -2,10 +2,10 @@
 #SBATCH -p general             # Partition (queue)
 #SBATCH -q public              # QoS
 #SBATCH -N 1                   # Number of nodes
-#SBATCH -n 2                   # Total MPI tasks
+#SBATCH -n 4                   # Total MPI tasks
 #SBATCH -c 1                   # Cores per task
-#SBATCH -t 0-00:01:00          # Time in D-HH:MM:SS (1 min for test)
-#SBATCH --job-name=openfoam-test
+#SBATCH -t 0-00:00:20          # Time in D-HH:MM:SS (1 min for test)
+#SBATCH --job-name=openfoam-lbf-test
 #SBATCH --output=job_output.log
 #SBATCH --error=job_error.log
 
@@ -13,18 +13,21 @@
 #module load apptainer
 
 # Define variables
-CASE_DIR=/home/mzomoro1/openfoam-container/case/pitzDaily
-SIF_PATH=/home/mzomoro1/openfoam-container/openfoam.sif
+CASE_DIR=/home/mzomoro1/openfoam-container/case/Plate2D
+SIF_PATH=/home/mzomoro1/openfoam-container/lbf.sif
 
 # Step 1: blockMesh
-apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-v2212/etc/bashrc && cd $CASE_DIR && blockMesh"
+apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-10/etc/bashrc && cd $CASE_DIR && blockMesh"
 
-# Step 2: decomposePar
-apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-v2212/etc/bashrc && cd $CASE_DIR && decomposePar"
+# Step 2: setFields 
+apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-10/etc/bashrc && cd $CASE_DIR && setFields"
 
-# Step 3: Run solver (example: icoFoam)
-srun --mpi=pmi2 apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-v2212/etc/bashrc && cd $CASE_DIR && simpleFoam -parallel"
+# Step 3: decomposePar
+apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-10/etc/bashrc && cd $CASE_DIR && decomposePar"
 
-# Step 4: Reconstruct (optional)
-apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-v2212/etc/bashrc && cd $CASE_DIR && reconstructPar"
+# Step 4: Run solver (example: icoFoam)
+srun --mpi=pmi2 apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-10/etc/bashrc && cd $CASE_DIR && laserbeamFoam -parallel"
+
+# Step 5: Reconstruct (optional)
+apptainer exec $SIF_PATH bash -c "source /opt/OpenFOAM/OpenFOAM-10/etc/bashrc && cd $CASE_DIR && reconstructPar"
 
